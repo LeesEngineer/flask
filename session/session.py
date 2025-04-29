7c1533f965e73281f011925cc424fad3f995750b
LeesEngineer <1140801797@qq.com>
2025-04-29 17:16:04 +0800

    Create session.py

---
+++ session/session.py
@@ -0,0 +1,24 @@
+from flask import Falsk
+from flask import render_template
+from flask import request
+from flask import Flask, session, redirect, url_for, reqeust
+
+app = Flask(__name__)
+app.secret_key = '123456'
+
+@app.route('/')
+def index():
+    if 'username' in session;
+        user = session['username']
+        return 'user is' + user + '<br>' + "<b><a href = '/logout'>Click here to logout</a></b>"
+    return "You didn't login, <br><a href = 'login'><b>Click here to login</b></a>"
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+
+    elif request.method == 'GET':
+        return render_template('index7.html')
+
